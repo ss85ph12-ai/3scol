71cc6389eefd4a7f3cce97508b89fd94db19a0e8
ss85ph12-ai <ss85ph12@gmail.com>
2026-06-01 17:49:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ const APP_CONFIG = {
     DB_KEY: 'skol_al_rhman_db',
     
     // 2. إصدار التطبيق (قم بتغييره لإجبار الأجهزة على تحديث النظام)
-    APP_VERSION: 'v1',
+    APP_VERSION: 'v11',
 
     // 3. إعدادات Firebase الخاصة بالنسخة
     FIREBASE_CONFIG: {
-        apiKey: "AIzaSyDueK3z2P9lTH79RSyRl40w1WFmznuCsUc",
-        authDomain: "scol-810e1.firebaseapp.com",
-        projectId: "scol-810e1",
-        storageBucket: "scol-810e1.firebasestorage.app",
-        messagingSenderId: "1012859853609",
-        appId: "1:1012859853609:web:d51d65088b8b5b5e703538"
+        apiKey: "AIzaSyDhfv1eQBCY-ZTdJ4q8N89G4NXi8WqxnQE",
+        authDomain: "scol-d3858.firebaseapp.com",
+        projectId: "scol-d3858",
+        storageBucket: "scol-d3858.firebasestorage.app",
+        messagingSenderId: "684296141264",
+        appId: "1:684296141264:web:511a36258bcc21c204e1d5"
     }
 };
